3c145af4e292b6cba9c4e412c6109d87553ba469
Gianluca200600 <gianlucaerbetta00@gmail.com>
2025-05-15 21:06:17 +0200

    Filters Working

--- filter.js
+++ filter.js
@@ -1,29 +1,27 @@
 export const filters = {
-    name: "",
-    playerNumber: "",
-    playingTime: ""
+	name: "",
+	playerNumber: "",
+	playingTime: ""
 }
 
 const resetFilters = () => {
-    filters.name = "";
-    filters.playerNumber = "";
-    filters.playingTime = "";
+	filters.name = "";
+	filters.playerNumber = "";
+	filters.playingTime = "";
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const nameElement = document.getElementById("name");
-    const playersElement = document.getElementById("player-number");
-    const playingTimeElement = document.getElementById("playing-time");
+const nameElement = document.getElementById("name");
+const playersElement = document.getElementById("player-number");
+const playingTimeElement = document.getElementById("playing-time");
 
-    nameElement.addEventListener("input", (event) => {
-        filters.name = event.target.value;
-    });
+nameElement.addEventListener("input", (event) => {
+	filters.name = event.target.value;
+});
 
-    playersElement.addEventListener("input", (event) => {
-        filters.playerNumber = event.target.value;
-    });
+playersElement.addEventListener("input", (event) => {
+	filters.playerNumber = event.target.value;
+});
 
-    playingTimeElement.addEventListener("input", (event) => {
-        filters.playingTime = event.target.value;
-    });
+playingTimeElement.addEventListener("input", (event) => {
+	filters.playingTime = event.target.value;
 });
