bbc83a04e72bdb01e0486a8036cc610b1c1e7491
Gianluca200600 <gianlucaerbetta00@gmail.com>
2025-06-04 15:57:20 +0200

    Min and Max Player Number Filters

--- filter.js
+++ filter.js
@@ -1,26 +1,34 @@
 export const filters = {
 	name: "",
 	description: "",
-	playerNumber: "",
+	minPlayerNumber: 0,
+	maxPlayerNumber: 0,
 	playingTime: ""
 }
 
 const resetFilters = () => {
 	filters.name = "";
-	filters.playerNumber = "";
+	filters.description = "";
+	filters.minPlayerNumber = 0;
+	filters.maxPlayerNumber
 	filters.playingTime = "";
 }
 
 const nameElement = document.getElementById("name");
-const playersElement = document.getElementById("player-number");
+const minPlayersElement = document.getElementById("min-player-number");
+const maxPlayersElement = document.getElementById("max-player-number");
 const playingTimeElement = document.getElementById("playing-time");
 
 nameElement.addEventListener("input", (event) => {
 	filters.name = event.target.value;
 });
 
-playersElement.addEventListener("input", (event) => {
-	filters.playerNumber = event.target.value;
+minPlayersElement.addEventListener("input", (event) => {
+	filters.minPlayerNumber = event.target.value;
+});
+
+maxPlayersElement.addEventListener("input", (event) => {
+	filters.maxPlayerNumber = event.target.value;
 });
 
 playingTimeElement.addEventListener("input", (event) => {
